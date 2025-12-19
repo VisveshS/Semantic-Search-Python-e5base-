9bd5b4e2a958772297883d84f1eb71652d22302d
visvesh <visveshsubra@gmail.com>
2025-12-19 14:44:01 +0530

    insertion without sentence transformer and rechecking dot products, efficient, non-blocking now

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 # <editor-fold desc="IMPORTS">
 import sys
-from contextlib import nullcontext
-from networkx.algorithms.reciprocity import reciprocity
-from networkx.classes import neighbors
-import numpy as np
 from txtai.embeddings import Embeddings
 import sqlite3
 import pickle
-import time
 import heapq
 # </editor-fold>
 
 # <editor-fold desc="FILE PATHS">
-model_path = r'C:\Users\visvesh\.cache\huggingface\hub\models--intfloat--e5-base\snapshots\b533fe4636f4a2507c08ddab40644d20b0006d6a';
+model_path = r'C:\Users\visvesh\.cache\huggingface\hub\models--intfloat--e5-base\snapshots\b533fe4636f4a2507c08ddab40644d20b0006d6a'
 embeddings_path = r'C:\Users\visvesh\.cache\txtai_embeddings'
-filename = r'C:\Users\visvesh\Documents\instagram index.txt';
+filename = r'C:\Users\visvesh\Documents\instagram index.txt'
 database = r'C:\Users\visvesh\Documents\store.db'
 in_knn_path = r'C:\Users\visvesh\Documents\DBoperations\in_knn.pkl'
 out_knn_path = r'C:\Users\visvesh\Documents\DBoperations\out_knn.pkl'
@@ -33,6 +28,7 @@ THRESHOLD = 7
 IN_KNN_CANDIDATE_NEIGHBOUR_INDEX = int(K*0.75)
 READ_BATCH_SIZE = 100
 REFRESH_BATCH_SIZE = 100
+TENTATIVE_KNN_FRACTION = 0.4
 # </editor-fold>
 
 # <editor-fold desc="LOAD DATA FROM STORAGE TO MEMORY">
@@ -55,7 +51,7 @@ refreshqueue = [set() for i in range(THRESHOLD+1)]
 # </editor-fold>
 
 # <editor-fold desc="NODE FUNCTIONS">
-def in_knn_update(id, old_knn, new_knn):
+def in_knn_update(nodeid, old_knn, new_knn):
     # assumes id node is still present, it is job of caller to ensure this
     dicta = dict(old_knn[:K])
     dictb = dict(new_knn[:K])
@@ -63,68 +59,60 @@ def in_knn_update(id, old_knn, new_knn):
     for i in old_knn_only:
         if i not in in_knn:
             continue
-        idx = next(index for index,nid in enumerate(in_knn[i]) if nid == id)
+        idx = next(index for index,nid in enumerate(in_knn[i]) if nid == nodeid)
         del in_knn[i][idx]
     for i in new_knn_only:
         if i not in out_knn:
             continue
-        in_knn[i].append(id)
+        in_knn[i].append(nodeid)
 
-def refreshnode(id):
-    old_knn = out_knn[id]
-    results = embeddings.search(db[id][1], K+DELTA+1, "dense")
+def refreshnode(nodeid):
+    old_knn = out_knn[nodeid]
+    results = embeddings.search(db[nodeid][1], K + DELTA + 1, "dense")
     new_knn = [(int(neighbour[0]), neighbour[1]) for neighbour in results[1:]]
-    in_knn_update(id, old_knn, new_knn)
-    out_knn[id] = new_knn
-
-def querytext(querystr):
-    return
+    in_knn_update(nodeid, old_knn, new_knn)
+    out_knn[nodeid] = new_knn
 
 def insert(new_internet_data):
     new_internet_data_id = max(in_knn)+1 if len(holes)==0 else heapq.heappop(holes)
-    new_internet_data_vector = embeddings.transform(new_internet_data)
     candidatelistlen = IN_KNN_CANDIDATE_LEN
     tentative_in_knn = list()
     while True:
         tentative_in_knn.clear()
-        bigquery=embeddings.search(new_internet_data, candidatelistlen, "dense", parameters={"efSearch": candidatelistlen})
-        vector_batch = embeddings.batchtransform([db[in_knn_candidate[0]][1] for in_knn_candidate in bigquery])
-        indices = [in_knn_candidate[0] for in_knn_candidate in bigquery]
-        in_knn_candidate_vectors = dict(zip(indices, vector_batch))
-        for i,in_knn_candidate in enumerate(bigquery):
-            cosine_similarity = np.dot(new_internet_data_vector, in_knn_candidate_vectors[in_knn_candidate[0]])
-            if out_knn[in_knn_candidate[0]][IN_KNN_CANDIDATE_NEIGHBOUR_INDEX][1]<cosine_similarity:
-                tentative_in_knn.append(in_knn_candidate[0])
-        if len(tentative_in_knn) < int(candidatelistlen*0.4):
+        bigquery = embeddings.search(new_internet_data, candidatelistlen, "dense", parameters={"efSearch": candidatelistlen})
+        bigquery = dict(bigquery)
+        for candidate_id in bigquery:
+            if out_knn[candidate_id][IN_KNN_CANDIDATE_NEIGHBOUR_INDEX][1] < bigquery[candidate_id]:
+                tentative_in_knn.append(candidate_id)
+        if len(tentative_in_knn) < int(candidatelistlen * TENTATIVE_KNN_FRACTION):
             break
         else:
             candidatelistlen *= 2
 
-    out_knn[new_internet_data_id] = bigquery[:K+DELTA]
+    out_knn[new_internet_data_id] = list(bigquery.items())[:K+DELTA]
     in_knn[new_internet_data_id] = []
     in_knn_update(new_internet_data_id, [], out_knn[new_internet_data_id])
     for i in tentative_in_knn:
         old_knn = out_knn[i].copy()
-        out_knn[i].append((new_internet_data_id, np.dot(new_internet_data_vector, in_knn_candidate_vectors[i])))
+        out_knn[i].append((new_internet_data_id, bigquery[i]))
         out_knn[i].sort(key=lambda x:x[1], reverse=True)
         out_knn[i] = out_knn[i][:K+DELTA]
-        #why not added to in_knn_of new node?
         in_knn_update(i, old_knn, out_knn[i])
     embeddings.upsert([(new_internet_data_id, new_internet_data)])
     db[new_internet_data_id]=["URL_PLACEHOLDER", new_internet_data]
 
-def delete(id):
+def delete(nodeid):
     # assumes node is present
-    heapq.heappush(holes, id)
-    in_knn_update(id, out_knn[id], [])
-    in_knn_id = in_knn[id]
-    del out_knn[id]
-    del in_knn[id]
-    embeddings.delete([id])
+    heapq.heappush(holes, nodeid)
+    in_knn_update(nodeid, out_knn[nodeid], [])
+    in_knn_id = in_knn[nodeid]
+    del out_knn[nodeid]
+    del in_knn[nodeid]
+    embeddings.delete([nodeid])
     #need to rollback these maybe
     #db updatte also
     for i in in_knn_id:
-        idx = next(j for j,(index,sim) in enumerate(out_knn[i]) if out_knn[i][j][0] == id)
+        idx = next(j for j,(_,sim) in enumerate(out_knn[i]) if out_knn[i][j][0] == nodeid)
         del out_knn[i][idx]
         if out_knn[i][K-1][0] in in_knn:
             in_knn[out_knn[i][K-1][0]].append(i)
@@ -145,37 +133,37 @@ def refreshbatch():
     for i in range(THRESHOLD+1):
         while len(ids)<REFRESH_BATCH_SIZE:
             if len(refreshqueue[i]) == 0:
-                break;
-            id = refreshqueue[i].pop()
-            if id in out_knn:
-                ids.append(id)
+                break
+            nodeid = refreshqueue[i].pop()
+            if nodeid in out_knn:
+                ids.append(nodeid)
     if len(ids)==0:
         return
     #batch of size 100 is pretty optimal. 1.5s
-    results = embeddings.batchsearch([db[id][1] for id in ids], K+DELTA+1, "dense")
+    results = embeddings.batchsearch([db[nodeid][1] for nodeid in ids], K+DELTA+1, "dense")
     for i in range(len(ids)):
         new_knn = [(int(neighbour[0]), neighbour[1]) for neighbour in results[i][1:]]
         in_knn_update(ids[i], out_knn[ids[i]], new_knn)
         out_knn[ids[i]]=new_knn
 
-def sanity(id):
-    K=40
-    for i,f in out_knn[id][:40]:
+def sanity(nodeid):
+    for i,f in out_knn[nodeid][:K]:
         if i not in in_knn:
-            return(f"{id} neighbour: {i} does not exist")
-        elif id not in in_knn[i]:
-            return(f"{id} not in in_knn[{i}]")
-    for i in in_knn[id]:
+            return f"{nodeid} neighbour: {i} does not exist"
+        elif nodeid not in in_knn[i]:
+            return f"{nodeid} not in in_knn[{i}]"
+    for i in in_knn[nodeid]:
         if i not in out_knn:
-            return(f"{id} in {i}'s knn but {i} does not exist")
-        elif id not in list(zip(*out_knn[i][:40]))[0]:
-            return(f"{id} not in out_knn[{i}]")
+            return f"{nodeid} in {i}'s knn but {i} does not exist"
+        elif nodeid not in list(zip(*out_knn[i][:40]))[0]:
+            return f"{nodeid} not in out_knn[{i}]"
     for i in out_knn:
         if i not in in_knn:
-            return(f"out_knn[{i}], but no in_knn[{i}]")
+            return f"out_knn[{i}], but no in_knn[{i}]"
     for i in in_knn:
         if i not in out_knn:
-            return(f"in_knn[{i}], but no out_knn[{i}]")
+            return f"in_knn[{i}], but no out_knn[{i}]"
+    return None
 # </editor-fold>
 
 # <editor-fold desc="I/O WITH JAVA SPRING BOOT (interactive)">
